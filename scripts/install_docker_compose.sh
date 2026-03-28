5f8c565612f33b981ae8bf37fbd87e399a8be020
dric2018 <manouancedric@gmail.com>
2026-03-28 01:50:00 +0000

    add langsmith tracking v2

--- scripts/install_docker_compose.sh
+++ scripts/install_docker_compose.sh
@@ -5,3 +5,4 @@ mkdir -p $DOCKER_CONFIG/cli-plugins
 curl -SL https://github.com/docker/compose/releases/download/v5.0.1/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
 chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
 docker compose version
+apt-get update
